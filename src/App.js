b3d8916d23f9c3e53b11dc378bad753400a316ff
Rayala369 <9022prasad.ktps@gmail.com>
2021-10-06 16:40:00 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+    const [ data , setdata] = useState({
+      title:"",
+      des:"",
+      select:"",
+      date:"",
+    }) 
+
+    const [todos,setTodos] = useState([{
+
+    }])
+
+
+    const changeHandler = e =>{
+      // setTask(e.target.value)
+      setdata({...data,[e.target.name]:[e.target.value]})
+    }
+    const submitHandler= e=>{
+      e.preventDefault();
+      // console.log(data);
+      const newTodos =[...todos,data];
+      setTodos(newTodos);
+      setdata("");
+
+     
+     
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <h2>From</h2>
+      <form onSubmit={submitHandler} >
+            <input type="text" required name="title" value={data.title} placeholder="Title"  onChange={changeHandler}/>
+            <input type="text" required  name="des" value={data.des} placeholder="Description" onChange={changeHandler} />
+            <label for="status"  required>Status:</label>
+            <select name="status" required name="select" value={data.select} id="status" onChange={changeHandler}>
+                  <option   value="ENUM">ENUM</option>
+                  <option  required  value="Open">Open</option>
+                  <option  required value="In-Progress">In-Progress</option>
+                  <option  required value="Completed">Completed</option>
+            </select>
+            <input type="datetime-local" name="date" required value={data.date} id="datastamp" onChange={changeHandler}></input>
+            <input type="submit" value="AddList" required name="Add" />
+            
+      </form>
+
+
+
+
+      {
+              
+              todos.map((element,index)=>(
+                <>
+                  
+                  <h1> {element.title} </h1>
+                  <h1> {element.des} </h1>
+                  <h1> {element.select} </h1>
+                  <h1> {element. date} </h1>
+                  <hr></hr>
+                </>
+              ))  
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
